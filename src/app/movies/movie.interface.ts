cc0c6080acf88cd73224e39fdb87fde4551275c5
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-07-08 17:27:24 +0200

    feat(home): add carousel

---
+++ src/app/movies/movie.interface.ts
@@ -0,0 +1,12 @@
+export interface MovieCreateInterface {
+  title:string,
+  description: string,
+  image_Url: string,
+  minimun_Age: number,
+  is_Favorite:boolean
+}
+
+export interface MovieUpdateInterface extends MovieCreateInterface {
+  id: string,
+  create_At: Date
+}
