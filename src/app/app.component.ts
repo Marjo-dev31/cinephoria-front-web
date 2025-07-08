cf8c9976efa59a509ef0d49685a26a4e0b03678d
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-07-08 09:57:49 +0200

    feat(footer): footer component done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   template: `<app-header />
-  <main><router-outlet /></main>`,
+  <main><router-outlet /></main>
+  <app-footer class="mt-auto"/>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
