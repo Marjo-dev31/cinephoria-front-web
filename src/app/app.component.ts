5c9c52d17e1d7ab6506e125e51ab2de92d62dcaf
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-07-09 12:02:41 +0200

    feat(carousel): carousel done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { FooterComponent } from './shared/component/footer/footer.component';
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   template: `<app-header />
   <main><router-outlet /></main>
-  <app-footer class="mt-auto"/>`,
+  <app-footer class="mt-20"/>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
--- src/app/movies/movies.service.ts
+++ src/app/movies/movies.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { MovieUpdateInterface } from './movie.interface';
 
 @Injectable({
     providedIn: 'root',
@@ -9,7 +11,7 @@ export class MoviesService {
     private readonly url = `${environment.serverUrl}/movies`;
     private readonly http = inject(HttpClient);
 
-    getAllMovies() {
-        return this.http.get(this.url);
+    getAllMovies(): Observable<MovieUpdateInterface[]> {
+        return this.http.get<MovieUpdateInterface[]>(this.url);
     }
 }
