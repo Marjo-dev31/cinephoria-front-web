4fa1fe274f19ead1bf7d4e530c2a1a7b92fb9194
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-07-10 15:21:08 +0200

    feat(movies): cards done

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
+import { MovieUpdateInterface } from './movie.interface';
+import { MoviesService } from './movies.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { environment } from '../../environments/environment';
 
 @Component({
-  selector: 'app-movies',
-  imports: [],
-  templateUrl: './movies.component.html',
-  styleUrl: './movies.component.css'
+    selector: 'app-movies',
+    imports: [],
+    templateUrl: './movies.component.html',
+    styleUrl: './movies.component.css',
 })
 export class MoviesComponent {
+    private readonly moviesService = inject(MoviesService);
 
+    // check if it works in production
+    url = `${environment.serverUrl}/uploads/`;
+
+    movies: Signal<MovieUpdateInterface[]> = toSignal(
+        this.moviesService.getAllMovies(),
+        { initialValue: [] },
+    );
 }
