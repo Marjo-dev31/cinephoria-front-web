d36db9823cff11aedef05cf20cab777eaef70a39
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-07-11 16:47:40 +0200

    feat(review): get random review

--- src/app/movies/movie.interface.ts
+++ src/app/movies/movie.interface.ts
@@ -1,15 +1,17 @@
 import { GenreInterface } from "./genre.interface"
+import { ReviewInterface } from "./review.interface"
 
 export interface MovieCreateInterface {
   title:string,
   description: string,
   image_Url: string,
   minimun_Age: number,
-  is_Favorite:boolean
+  is_Favorite: boolean
 }
 
 export interface MovieUpdateInterface extends MovieCreateInterface {
   id: string,
   create_At: Date,
-  genre: GenreInterface
+  genre: GenreInterface,
+  reviews: ReviewInterface[]
 }
