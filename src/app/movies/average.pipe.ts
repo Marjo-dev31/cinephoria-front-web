57975a4ddd7a55ccb8fa242d1f23e4ca9f114bf7
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-07-11 17:36:16 +0200

    feat(review): get average grade

---
+++ src/app/movies/average.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from "@angular/core";
+import { ReviewInterface } from "./review.interface";
+
+@Pipe({
+  name:'averagePipe'
+})
+export class AveragePipe implements PipeTransform {
+  transform(reviews: ReviewInterface[]): number {
+    if(reviews.length === 0) {
+      return 0
+    }
+    const arrayGrades = reviews.map((review)=> review.grade)
+     const average = arrayGrades.reduce((acc, cur)=> acc + cur, 
+    0) / arrayGrades.length
+     return average
+  }
+}
