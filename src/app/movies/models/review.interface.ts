eb2e423aeceb2bfbf70a170b70b5de50b5bec23c
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-08-04 11:11:23 +0200

    fix(review): implement interface

--- src/app/movies/models/review.interface.ts
+++ src/app/movies/models/review.interface.ts
@@ -1,7 +1,12 @@
-export interface ReviewInterface {
-  id?: string,
-  description: string,
-  grade: number,
-  is_Validated: boolean,
-  movie: string
+export interface ReviewCreateInterface {
+    description: string;
+    grade: number;
+    is_Validated: boolean;
+    movie: string;
 }
+
+export interface ReviewUpdateInterface extends ReviewCreateInterface {
+    id: string;
+}
+
+export interface ReviewInterface extends ReviewUpdateInterface {}
