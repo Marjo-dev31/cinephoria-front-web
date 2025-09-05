94b538c7c62e1e81d179f09395f094f0df032452
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-09-05 20:46:13 +0200

    feat(user): user backoffice create

---
+++ src/app/user/features/user-backoffice/user-backoffice.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-backoffice',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-backoffice.component.html',
+  styleUrl: './user-backoffice.component.css'
+})
+export class UserBackofficeComponent {
+
+}
