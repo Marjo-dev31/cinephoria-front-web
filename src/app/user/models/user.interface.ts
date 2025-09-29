ee520b7ac3eaf6041d44b7770b371f984693dfa3
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-09-29 10:51:48 +0200

    feat(admin): backoffice admin done

--- src/app/user/models/user.interface.ts
+++ src/app/user/models/user.interface.ts
@@ -1,32 +1,18 @@
-import { ReviewInterface } from "../../reviews/models/review.interface";
-import { RoleInterface } from "./role.interface";
+import { ReviewInterface } from '../../reviews/models/review.interface';
+import { RoleInterface } from './role.interface';
 
-export interface AllUserCreateInterface {
-  firstname: string;
-  lastname: string;
-  mail: string;
-  password: string;
+export interface UserCreateInterface {
+    firstname: string;
+    lastname: string;
+    mail: string;
+    username: string;
+    role: RoleInterface;
+    password: string;
 }
-
-export interface EmployeeCreateInterface extends AllUserCreateInterface {
-  role: RoleInterface;
-}
-
-export interface EmployeeUpdateInterface extends EmployeeCreateInterface {
-  id: string
-}
-
-export interface EmployeeInterface extends EmployeeUpdateInterface {
-}
-
-export interface UserCreateInterface extends AllUserCreateInterface{
-  username: string;
-}
-
 export interface UserUpdateInterface extends UserCreateInterface {
-  id: string
+    id: string;
 }
 
 export interface UserInterface extends UserUpdateInterface {
-  reviews: ReviewInterface
+    reviews: ReviewInterface;
 }
